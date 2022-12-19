10473cf3ae90780d792374ad93a0672be2538ce4
Dheeya17 <86183500+Dheeya17@users.noreply.github.com>
2022-12-19 17:07:38 +0700

    init all

---
+++ src/routes/EditPhoto.jsx
@@ -0,0 +1,85 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const EditPhoto = () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [captions, setCaptions] = useState("");
+  const [photo, setPhoto] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const editPhoto = (e) => {
+    e.preventDefault();
+    setLoading(true)
+    const update = {
+      ...photo,
+      imageUrl: imageUrl,
+      captions: captions,
+      updatedAt: new Date().toISOString()
+    }
+    fetch(`https://gallery-app-server.vercel.app/photos/${id}`,{
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(update)
+    }).then(resp => {resp.json();
+      navigate('/photos')});
+    setLoading(false)
+  };
+  
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`http://localhost:3001/photos/${id}`)
+    .then(resp => resp.json())
+    .then(data => {
+      setImageUrl(data.imageUrl)
+      setCaptions(data.captions)
+    });
+    setLoading(false);
+    // TODO: answer here
+  }, [id]);
+
+  if (error) return <div>Error!</div>;
+
+  return (
+    <>
+      {loading ? (
+        <h1 style={{ width: "100%", textAlign: "center", marginTop: "20px" }}>
+          Loading...
+        </h1>
+      ) : (
+        <div className="container">
+          <form className="edit-form" onSubmit={editPhoto}>
+            <label>
+              Image Url:
+              <input
+                className="edit-input"
+                type="text"
+                value={imageUrl}
+                onChange={(e) => setImageUrl(e.target.value)}
+              />
+            </label>
+            <label>
+              Captions:
+              <input
+                className="edit-input"
+                type="text"
+                value={captions}
+                data-testid="captions"
+                onChange={(e) => setCaptions(e.target.value)}
+              />
+            </label>
+            <input className="submit-btn" type="submit" value="Submit" data-testid="submit" />
+          </form>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default EditPhoto;
